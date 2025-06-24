ec2f00c2b214a1a56f9647c3c1bf6849251f8e6f
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-24 20:41:24 +0530

    Updated code files via regeneration: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,7 +29,8 @@ export default {
 				foreground: 'hsl(var(--foreground))',
 				primary: {
 					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))'
+					foreground: 
+'hsl(var(--primary-foreground))'
 				},
 				secondary: {
 					DEFAULT: 'hsl(var(--secondary))',
